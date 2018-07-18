ed2007e146235841d23fc32debeff85a9640ed04
Tim Renner <renner.timothy@gmail.com>
2018-07-17 20:45:56 -0500

    Wrote transformer for new grammar.

--- svl/cli.py
+++ svl/cli.py
@@ -1,11 +1,16 @@
 import click
 import svl
+import json
 
 
 @click.command()
 @click.argument("svl_source", type=click.File('r'))
-def cli(svl_source):
+@click.option("--debug", is_flag=True)
+def cli(svl_source, debug):
 
     svl_string = svl_source.read()
 
-    print(svl.parse_svl(svl_string).pretty())
+    if debug:
+        print(svl.parse_svl(svl_string, debug=True).pretty())
+    else:
+        print(json.dumps(svl.parse_svl(svl_string), indent=4, sort_keys=True))
--- svl/svl.py
+++ svl/svl.py
@@ -1,10 +1,92 @@
 import lark
 import pkg_resources
 
+from toolz import merge, partition_all
+
+
+class SVLTransformer(lark.Transformer):
+
+    def visualization(self, items):
+        return merge(*items)
+
+    def datasets(self, items):
+        return {
+            "datasets": dict(
+                map(
+                    lambda x: (str(x[0]), x[1][1:-1]),
+                    partition_all(2, items)
+                )
+            )
+        }
+
+    def charts(self, items):
+        return {
+            "vcat": items
+        }
+
+    def vcat(self, items):
+        return {
+            "vcat": items
+        }
+
+    def chart(self, items):
+        print(items)
+        return merge(*items)
+
+    def mark2d(self, items):
+        return {"type": str(items[0]).lower()}
+
+    def mark1d(self, items):
+        return {"type": str(items[0]).lower()}
+
+    def data(self, items):
+        return {"data": str(items[0])}
+
+    def mark1d_opts(self, items):
+        return merge(*items)
+
+    def bins(self, items):
+        return {"bins": int(items[0])}
+
+    def step(self, items):
+        return {"step": float(items[0])}
+
+    def dimensions(self, items):
+        return merge(*items)
+
+    def x(self, items):
+        return {"x": merge(*items)}
+
+    def y(self, items):
+        return {"y": merge(*items)}
+
+    def color(self, items):
+        return {"color": merge(*items)}
+
+    def field(self, items):
+        return {"field": str(items[0])}
+
+    def temporal(self, items):
+        return {"temporal": str(items[0])}
+
+    def aggregation(self, items):
+        return {"agg": str(items[0])}
+
+
 debug_parser = lark.Lark(
     pkg_resources.resource_string("resources", "svl.lark").decode("utf-8")
 )
 
 
-def parse_svl(svl_string):
-    return debug_parser.parse(svl_string)
+parser = lark.Lark(
+    pkg_resources.resource_string("resources", "svl.lark").decode("utf-8"),
+    parser="lalr",
+    transformer=SVLTransformer()
+)
+
+
+def parse_svl(svl_string, debug=False):
+    if debug:
+        return debug_parser.parse(svl_string)
+    else:
+        return parser.parse(svl_string)
