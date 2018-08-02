adfc4e92de11a95c80d711e7916c0c7ae6109ad7
Tim Renner <renner.timothy@gmail.com>
2018-08-02 05:56:19 -0500

    Implemented simple test cases for tree_to_grid.

--- test/test_layout.py
+++ test/test_layout.py
@@ -1,6 +1,7 @@
 from svl.layout import (
     shift_node_position,
-    shift_tree_positions
+    shift_tree_positions,
+    tree_to_grid
 )
 
 
@@ -104,5 +105,155 @@ def test_shift_tree_positions():
         row_stretch,
         column_stretch
     )
+
+    assert truth == answer
+
+
+def test_tree_to_grid_single_plot_vcat():
+    """ Tests that the tree_to_grid function works for a single plot.
+    """
+    tree = {
+        "vcat": [{
+            "plot": 1
+        }]
+    }
+
+    answer = {
+        "cat": None,
+        "nodes": [{
+            "cat": "vcat",
+            "nodes": [{
+                "plot": 1,
+                "row_start": 0,
+                "row_end": 1,
+                "column_start": 0,
+                "column_end": 1
+            }]
+        }]
+    }
+
+    truth = tree_to_grid(tree)
+
+    assert truth == answer
+
+
+def test_tree_to_grid_single_plot_hcat():
+    """ Tests that the tree_to_grid function works correctly for a single
+        hcat plot.
+    """
+    tree = {
+        "hcat": [{
+            "plot": 1
+        }]
+    }
+
+    truth = {
+        "cat": None,
+        "nodes": [{
+            "cat": "hcat",
+            "nodes": [{
+                "plot": 1,
+                "row_start": 0,
+                "row_end": 1,
+                "column_start": 0,
+                "column_end": 1
+            }]
+        }]
+    }
+
+    answer = tree_to_grid(tree)
+
+    assert truth == answer
+
+
+def test_tree_to_grid_multiple_plots_vcat():
+    """ Tests that the tree_to_grid function returns the correct value when the
+        tree contains multiple vcat plots.
+    """
+    tree = {
+        "vcat": [
+            {"plot": 1},
+            {"plot": 2}
+        ]
+    }
+
+    truth = {
+        "cat": None,
+        "nodes": [
+            {
+                "cat": "vcat",
+                "nodes": [{
+                    "plot": 1,
+                    "row_start": 0,
+                    "row_end": 1,
+                    "column_start": 0,
+                    "column_end": 1
+                }]
+            }, {
+                "cat": "vcat",
+                "nodes": [{
+                    "plot": 2,
+                    "row_start": 1,
+                    "row_end": 2,
+                    "column_start": 0,
+                    "column_end": 1
+                }]
+            }
+        ]
+    }
+
+    answer = tree_to_grid(tree)
+
+    assert truth == answer
+
+
+def test_tree_to_grid_multiple_plots_hcat():
+    """ Tests that the tree_to_grid function returns the correct value when
+        there are multiple horizontally concatenated plots.
+    """
+
+    tree = {
+        "hcat": [
+            {"plot": 1},
+            {"plot": 2},
+            {"plot": 3}
+        ]
+    }
+
+    truth = {
+        "cat": None,
+        "nodes": [
+            {
+                "cat": "hcat",
+                "nodes": [{
+                    "plot": 1,
+                    "row_start": 0,
+                    "row_end": 1,
+                    "column_start": 0,
+                    "column_end": 1
+                }]
+            }, {
+                "cat": "hcat",
+                "nodes": [{
+                    "plot": 2,
+                    "row_start": 0,
+                    "row_end": 1,
+                    "column_start": 1,
+                    "column_end": 2
+                }]
+            }, {
+                "cat": "hcat",
+                "nodes": [{
+                    "plot": 3,
+                    "row_start": 0,
+                    "row_end": 1,
+                    "column_start": 2,
+                    "column_end": 3
+                }]
+            }
+        ]
+    }
+
+    answer = tree_to_grid(tree)
 
     assert truth == answer
