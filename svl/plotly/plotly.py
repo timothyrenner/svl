e1578a09e3129c8f1fcbdd7bf5562235888354a8
Tim Renner <renner.timothy@gmail.com>
2018-08-23 17:56:49 -0500

    Added functions for grabbing the template and required variables for plotly plots.

--- svl/plotly/plotly.py
+++ svl/plotly/plotly.py
@@ -1,4 +1,7 @@
-from toolz import merge
+from toolz import merge, compose, pluck
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+listpluck = compose(list, pluck)
 
 
 def plotly_histogram(svl_plot, data):
@@ -52,3 +55,69 @@ def plotly_scatter(svl_plot, data):
 
 def plotly_boxplot(svl_plot, data):
     pass  # TODO: Implement.
+
+
+PLOTLY_PLOTS = {
+    "histogram": plotly_histogram,
+    "bar": plotly_bar,
+    "line": plotly_line,
+    "scatter": plotly_scatter,
+    "boxplot": plotly_boxplot
+}
+
+
+def plotly_template_vars(svl_plots, datas):
+    """ Constructs the variables needed for the plotly template from the
+        SVL plots and their associated data values.
+
+        Parameters
+        ----------
+        svl_plots : list
+            A list of dicts defining the SVL plots in the format produced by
+            the layout module.
+        datas : list
+            A list of dicts defining the data required for each plot in the
+            format produced by the data module.
+
+        Returns
+        -------
+        dict
+            A dictionary of the keyword arguments required to render the plotly
+            template. Specifically, num_rows, num_columns, and plots, which
+            contain (respectively) the number of grid rows, the number of grid
+            columns, and the plotly plots.
+    """
+    num_rows = max(listpluck("row_end", svl_plots)) - 1
+    num_columns = max(listpluck("column_end", svl_plots)) - 1
+
+    plots = [
+        {
+            "row_start": plot["row_start"],
+            "row_end": plot["row_end"],
+            "column_start": plot["column_start"],
+            "column_end": plot["column_end"],
+            "plotly": PLOTLY_PLOTS[plot["type"]](plot, data)
+        } for plot, data in zip(svl_plots, datas)
+    ]
+
+    return {
+        "num_rows": num_rows,
+        "num_columns": num_columns,
+        "plots": plots
+    }
+
+
+def plotly_template():
+    """ Obtains the template for plotly plots.
+
+        Returns
+        -------
+        `jinja2.Template`
+            The tempate for plotly plots.
+    """
+    env = Environment(
+        loader=PackageLoader("svl.plotly", "templates"),
+        autoescape=select_autoescape(["html"])
+    )
+
+    return env.get_template("index.jinja")
--- test/test_plotly.py
+++ test/test_plotly.py
@@ -1,6 +1,10 @@
 import pytest
 
-from svl.plotly.plotly import plotly_histogram
+from svl.plotly.plotly import (
+    plotly_histogram,
+    plotly_template,
+    plotly_template_vars
+)
 
 
 @pytest.fixture
@@ -80,3 +84,90 @@ def test_plotly_histogram_bins(appended_data):
     answer = plotly_histogram(svl_plot, appended_data)
 
     assert truth == answer
+
+
+def test_plotly_template_vars(appended_data):
+    """ Tests that the plotly_template_vars function returns the correct value.
+    """
+    svl_plots = [
+        {
+            "row_start": 1,
+            "row_end": 2,
+            "column_start": 1,
+            "column_end": 3,
+            "type": "histogram",
+            "field": "temperature",
+            "bins": 25
+        }, {
+            "row_start": 2,
+            "row_end": 3,
+            "column_start": 1,
+            "column_end": 2,
+            "type": "histogram",
+            "field": "temperature",
+            "bins": 15
+        }, {
+            "row_start": 2,
+            "row_end": 3,
+            "column_start": 2,
+            "column_end": 3,
+            "type": "histogram",
+            "field": "temperature",
+            "bins": 10
+        }
+    ]
+
+    datas = [appended_data]*3
+
+    truth = {
+        "num_rows": 2,
+        "num_columns": 2,
+        "plots": [
+            {
+                "row_start": 1,
+                "row_end": 2,
+                "column_start": 1,
+                "column_end": 3,
+                "plotly": [{
+                    "type": "histogram",
+                    "x": [98, 102, 94],
+                    "nbinsx": 25
+                }]
+            }, {
+                "row_start": 2,
+                "row_end": 3,
+                "column_start": 1,
+                "column_end": 2,
+                "plotly": [{
+                        "type": "histogram",
+                        "x": [98, 102, 94],
+                        "nbinsx": 15
+                }]
+            }, {
+                "row_start": 2,
+                "row_end": 3,
+                "column_start": 2,
+                "column_end": 3,
+                "plotly": [{
+                    "type": "histogram",
+                    "x": [98, 102, 94],
+                    "nbinsx": 10
+                }]
+            }
+        ]
+    }
+
+    answer = plotly_template_vars(svl_plots, datas)
+
+    assert truth == answer
+
+
+def test_plotly_template():
+    """ Tests that the plotly_template function returns the correct value.
+    """
+
+    answer = plotly_template()
+
+    truth = "index.jinja"
+
+    assert truth == answer.name
