3acbdbf99a25f0f98982fc25e10e9e92e59abfe9
Tim Renner <renner.timothy@gmail.com>
2019-03-05 20:35:06 -0600

    Implemented several more validators.

--- svl/plot_validators.py
+++ svl/plot_validators.py
@@ -1,12 +1,67 @@
+from toolz import get
+
 PLOT_VALIDATORS = [
     (
         {"line", "scatter", "bar"},
         lambda x: ("x" not in x) or ("y" not in x),
         "XY plot does not have X and Y."
-    ), (
+    ),
+    (
         {"histogram"},
         lambda x: ("step" in x) and ("bins" in x),
         "Histogram cannot have STEP and BINS."
+    ),
+    (
+        {"line", "scatter", "bar"},
+        lambda x: ("agg" in x["x"]) and ("agg" in x["y"]),
+        "XY plot cannot have an aggregation on X and Y."
+    ),
+    (
+        {"histogram", "pie"},
+        lambda x: (
+            "agg" in get("x", x, {})
+        ) or (
+            "agg" in get("y", x, {})
+        ) or (
+            "agg" in get("axis", x, {})
+        ),
+        "Histograms and pie charts cannot have aggregations."
+    ),
+    (
+        {"histogram", "pie"},
+        lambda x: (
+            "temporal" in get("x", x, {})
+        ) or (
+            "temporal" in get("y", x, {})
+        ) or (
+            "temporal" in get("axis", x, {})
+        ),
+        "Histograms and pie charts cannot have temporal axes."
+    ),
+    (
+        {"histogram"},
+        lambda x: ("x" in x) and ("y" in x),
+        "Histograms can have X or Y, not both."
+    ),
+    (
+        {"histogram"},
+        lambda x: ("x" not in x) and ("y" not in x),
+        "Histograms must have an X or Y."
+    ),
+    (
+        {"pie"},
+        lambda x: "axis" not in x,
+        "Pie charts must have an axis."
+    ),
+    (
+        {"line", "bar"},  # SORT is a no-op for scatter.
+        lambda x: ("sort" in x["x"]) and ("sort" in x["y"]),
+        "Cannot sort by two axes."
+    ),
+    (
+        {"pie"},
+        lambda x: (get("hole", x, 0.0) < 0) or (get("hole", x, 0.0) > 1),
+        "HOLE must be between zero and one."
     )
 ]
 
--- test/test_plot_validators.py
+++ test/test_plot_validators.py
@@ -64,3 +64,184 @@ def test_validate_plot_histogram_cannot_have_step_and_bins():
 
     assert truth_ok == ok
     assert truth_message == message
+
+
+def test_validate_plot_xy_cannot_have_aggs_on_x_and_y():
+    """ Tests that the validate_Plot function returns the correct value when
+        the plot is an XY plot that has an agg on X and Y.
+    """
+    svl_plot = {
+        "type": "scatter",
+        "data": "bigfoot",
+        "x": {
+            "field": "latitude",
+            "agg": "MAX"
+        },
+        "y": {
+            "field": "humidity",
+            "agg": "MIN"
+        }
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "XY plot cannot have an aggregation on X and Y."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_histogram_and_pie_cannot_have_aggs():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot is a histogram or pie chart with an aggregation.
+    """
+    svl_plot = {
+        "type": "pie",
+        "data": "bigfoot",
+        "axis": {
+            "field": "classification",
+            "agg": "COUNT"
+        }
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Histograms and pie charts cannot have aggregations."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_histogram_and_pie_cannot_have_temporals():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot is a histogram or pie chart with a temporal axis.
+    """
+    svl_plot = {
+        "type": "histogram",
+        "data": "bigfoot",
+        "x": {
+            "field": "date",
+            "temporal": "YEAR"
+        }
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Histograms and pie charts cannot have temporal axes."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_histogram_cannot_have_x_and_y():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot is a histogram with x and y.
+    """
+    svl_plot = {
+        "type": "histogram",
+        "data": "bigfoot",
+        "x": {
+            "field": "humidity"
+        },
+        "y": {
+            "field": "wind_speed"
+        }
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Histograms can have X or Y, not both."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_histogram_must_have_x_or_y():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot is a histogram without an X or Y.
+    """
+    svl_plot = {
+        "type": "histogram",
+        "data": "bigfoot",
+        "bins": 10
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Histograms must have an X or Y."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_pie_must_have_axis():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot is a pie chart without an AXIS.
+    """
+    svl_plot = {
+        "type": "pie",
+        "data": "bigfoot",
+        "hole": 0.3
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Pie charts must have an axis."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_cannot_sort_on_both_axes():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot has sort on X and Y.
+    """
+    svl_plot = {
+        "type": "line",
+        "data": "bigfoot",
+        "x": {
+            "field": "date",
+            "temporal": "YEAR",
+            "sort": "DESC"
+        }, "y": {
+            "field": "classification",
+            "agg": "COUNT",
+            "sort": "ASC"
+        }
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "Cannot sort by two axes."
+
+    assert truth_ok == ok
+    assert truth_message == message
+
+
+def test_validate_plot_hole_not_between_zero_and_one():
+    """ Tests that the validate_plot function returns the correct value when
+        the plot has a HOLE that's not between zero and one.
+    """
+    svl_plot = {
+        "type": "pie",
+        "data": "bigfoot",
+        "axis": {
+            "field": "has_location"
+        },
+        "hole": 1.2
+    }
+
+    ok, message = validate_plot(svl_plot)
+
+    truth_ok = False
+    truth_message = "HOLE must be between zero and one."
+
+    assert truth_ok == ok
+    assert truth_message == message
