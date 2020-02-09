f6ab5ba45ba3e2a463fe32a0e98ad7f60c2a9b80
Tim Renner <renner.timothy@gmail.com>
2020-02-09 15:35:09 -0600

    Walk back pandas 1.0 because it doesn't support 3.5, add an uninstally pyarrow command because I think that is causing issues with the build.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         # and upgrade with caution.
         "lark-parser==0.6.6",
         "Jinja2>=2.10.1",
-        "pandas>=1.0.0,<2.0.0",
+        "pandas>=0.25.0,<1.0.0",
         "importlib-resources>=1.0.2,<2",
     ],
     extras_require={"parquet": ["pyarrow>=0.16.0"]},
